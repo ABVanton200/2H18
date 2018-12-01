5e3f73470dba81dd53ff28ed32086acba59419c5
Anton Babakhin <anton-098+ya@yandex.ru>
2018-12-02 01:47:39 +0300

    first commit

--- index2.js
+++ index2.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
-const env = require('dotenv').load();
 
 
 let items;
@@ -12,20 +11,21 @@ const PORT = 4321;
 const URL = 'https://kodaktor.ru/j/users';
 const app = express();
 const checkAuth = (r, res, next) => {
-	if (r.user && r.user.id) {    
+	if (r.user && r.user.id) {
 		next();
-	} else {    
+	} else {
 		res.redirect('/login/github');
 	}
 }
 
 passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    clientID: 'd03dc801f09d9c1eea6c',
+    clientSecret: '6dc26a45c0f424750d093fab04c9647099d663f5',
     callbackURL: "http://127.0.0.1:4321/login/github/callback"
   },
-  function(accessToken, refreshToken, profile, cb) {	  
-    return cb(null, profile);    
+  function(accessToken, refreshToken, profile, cb) {
+	  //console.log(profile.id);    
+      return cb(null, profile);    
   }
 ));
 
@@ -39,24 +39,38 @@ passport.deserializeUser(function(obj, cb) {
 
 app
 	.use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))	
-	.use(require('cookie-parser')())
-	.use(session({	secret:	'mysecret',	resave:	true,	saveUninitialized:	true }))
+    .use(bodyParser.urlencoded({ extended: true }))
 	.use(passport.initialize())
-  .use(passport.session())
-  .get('/', r => r.res.end('Homepage'))
+	.use(passport.session())
+	//.use(session({	secret:	'mysecret',	resave:	true,	saveUninitialized:	true }))
 	.get('/users', checkAuth, async r => r.res.render('list', { title: 'Список логинов', items }))
 	.get('/login', r => r.res.render('login'))
 	.get('/login/github', passport.authenticate('github'))
 	.get('/login/github/callback', 
-        passport.authenticate('github', { failureRedirect: '/login' }),
+        passport.authenticate('github', { failureRedirect: '/login/github' }),
         function(req, res) {    
-            res.redirect('/users');
-        })		
-  .get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
-  })
+            res.redirect('/');
+        })
+		/*
+	.post('/login/check', r => {
+		const { login: l } = r.body;
+		const user = items.find(({ login }) => login === l);
+		if (user) {
+			if (user.password === r.body.pass) {
+				r.session.auth = 'ok';
+				r.res.send('Good');
+			} else {
+				r.res.send('Wrong');
+			}
+		} else {
+			r.res.send('No such user');
+		}
+	})
+	*/
+	.get('/logout', r => {
+		r.session.destroy();
+		r.res.render('login');		
+	})
 	.use(r => r.res.status(404).end('Error 404'))
 	.use((e, r, res, n) => res.status(500).end(`Error 500 ${e}`))
 	.set('view engine', 'pug')
