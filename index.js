bc7eadeefda0add5caafbcf0fd1d7b9f9aa9c930
Anton Babakhin <anton-098+ya@yandex.ru>
2018-12-08 02:22:51 +0300

    first commit

--- index.js
+++ index.js
@@ -1,68 +1,33 @@
-const express = require('express');
-const { get } = require('axios');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const GitHubStrategy = require('passport-github').Strategy;
-const env = require('dotenv').load();
+const http = require('http');
 
+const URL = 'http://kodaktor.ru/api2/buffer2/';
 
-let items;
-const PORT = 4321;
-const URL = 'https://kodaktor.ru/j/users';
-const app = express();
-const checkAuth = (r, res, next) => {
-	if (r.user && r.user.id) {    
-		next();
-	} else {    
-		res.redirect('/login/github');
-	}
+const pget = (url) => {
+  return new Promise((res, rej) => {
+    http.get(url, (rdStr, buf = '', V2 = 0) => {
+      rdStr.on('data', d => {
+        if(String(d).search('cats') !== -1) {
+          V2 = buf.length;
+        }
+        buf+=d;            
+      });
+      rdStr.on('end', () => {
+        console.log("length = " + buf.length);
+        res({
+          length: buf.length,
+          V2
+        })
+      });
+    })
+  })
 }
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:4321/login/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {	  
-    return cb(null, profile);    
-  }
-));
+for(let i = 333; i <= 33333; i++) {
+  (async () => {
+    const res = await pget(URL + i);
+    console.log("i = " + i + "\nlength = " + res.length + "\nV2 = " + res.V2);
+  })();
+}
 
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-app
-	.use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))	
-	.use(require('cookie-parser')())
-	.use(session({	secret:	'mysecret',	resave:	true,	saveUninitialized:	true }))
-	.use(passport.initialize())
-  .use(passport.session())
-  .get('/', r => r.res.end('Homepage'))
-	.get('/users', checkAuth, async r => r.res.render('list', { title: 'Список логинов', items }))
-	.get('/login', r => r.res.render('login'))
-	.get('/login/github', passport.authenticate('github'))
-	.get('/login/github/callback', 
-        passport.authenticate('github', { failureRedirect: '/login' }),
-        function(req, res) {    
-            res.redirect('/users');
-        })		
-  .get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
-  })
-	.use(r => r.res.status(404).end('Error 404'))
-	.use((e, r, res, n) => res.status(500).end(`Error 500 ${e}`))
-	.set('view engine', 'pug')
-	.listen(process.env.PORT || PORT, async () => {
-		console.log('Start');
-		({ data: { users: items } } = await get(URL));
-	});
-	
-	
+// V1 = 65536
+// V2 = 917504
